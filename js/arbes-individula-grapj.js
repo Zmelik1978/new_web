8b7b337c7157c5794068539a608d6bf117a2285c
Richard Změlík <80571221+Zmelik1978@users.noreply.github.com>
2026-09-04 20:14:07 +0200

    smazána chybný JS u arbese

--- js/arbes-individula-grapj.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const notice = document.getElementById('notice');
-    if (notice) {
-        notice.textContent = 'Zpráva nebo upozornění se zobrazí zde.';
-    }
-});
